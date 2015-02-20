11c14e5f11651ff2224b74106506adabb269961f
ameri <mo_amer@encs.concordia.ca>
2015-02-20 17:39:21 -0500

    making files and mark file in the same folder next after marking sum and reconstruct EAS folder structure

--- make_mark_dir.py
+++ make_mark_dir.py
@@ -1,121 +1,163 @@
 import sys, os
 
+import shutil
+
+
 def read_question_parts(p_file_name):
-    file_h = open(p_file_name)
+
+    file_name = os.path.join(os.getcwd(), p_file_name)
+    file_h = open(file_name)
     lines_read = file_h.readlines()
     q_list = []	
     for q in lines_read:
-	q_split = q.split(':')
-	question =  q_split[0]
+        q_split = q.split(':')
+        question =  q_split[0]
 
-	parts = q_split[1].split()
-	#print parts
-	
-	q_list.append([question, parts])
+        parts = q_split[1].split()
+    #print parts
+
+        q_list.append([question, parts])
 
     file_h.close()
     return q_list
+
+def make_mark_file(mark_file_name, student_item, submission_item, time_snap,question_list):
+
+        file_h = open(mark_file_name, 'w')
+
+        str_header = 'ID:{0}  A#:{1}  @{2}\n'.format(student_item, submission_item, time_snap)
+
+        str_seprator = '*'*len(str_header) + '\n'
+        #str_seprator = ''
+
+        half_len = len(str_header) / 2
+
+        str_half_sep = '-'* half_len + '\n'
+        str_half_space = ' '*half_len
+        #str_half_space = ''
+
+
+        file_h.write(str_seprator)
+        file_h.write(str_header)
+
+        file_h.write(str_seprator)
+
+        for q in question_list:
+            str_q = '{2}{0}\n{1}'.format(q[0], str_seprator, str_half_space)
+            file_h.write(str_q)
+
+            for part in q[1]:
+                str_part = '({0})\n\n'.format(part)
+                file_h.write(str_part)
+
+            #file_h.write(str_q)
+
+            file_h.write('\n' + str_seprator)
+
+        str_total = 'sum = \n{0}'.format(str_seprator)
+        file_h.write(str_total)
+        file_h.close()
+
+
 
 def main(p_source_dir, p_target_dir, p_q_file):
 
     p_source_dir = os.path.abspath(p_source_dir)
     p_target_dir = os.path.abspath(p_target_dir)
-    
-    p_questions = 8
+
+
     question_list = read_question_parts(p_q_file)
 
     print question_list
+
 
     if not os.path.exists(p_target_dir):
         print 'creating path', p_target_dir
         os.mkdir(p_target_dir)
 
-#    return
 
     student_list = [item_prop for item_prop in os.listdir(p_source_dir)
                     if os.path.isdir(os.path.join(p_source_dir, item_prop))]	
-    #print student_list
-    
+
+    print 'students num: ' , len(student_list)
+
+
+
     for student_item in student_list:
-        student_path = os.path.join(p_target_dir, student_item)
+        #student_path = os.path.join(p_target_dir, student_item)
         student_source_path = os.path.join(p_source_dir, student_item)
 
-        if not os.path.exists(student_path):
-
-            os.mkdir(student_path)
 
 #        print student_source_path 
         student_submission_list = [s_item for s_item in os.listdir(student_source_path) \
-		if os.path.isdir(os.path.join(student_source_path, s_item))]
+        if os.path.isdir(os.path.join(student_source_path, s_item))]
+
+        #print student_item, ':',  len(student_submission_list)
 
 
-	for submission_item in student_submission_list :
+        for submission_item in student_submission_list :
             submission_source = os.path.join(student_source_path, submission_item)
 
-            submission_target = os.path.join(student_path, submission_item)
+            time_snap_list = [s_item for s_item in os.listdir(submission_source) \
+            if os.path.isdir(os.path.join(submission_source, s_item))]
 
-	    if not os.path.exists(submission_target):
-		os.mkdir(submission_target)
+               #print len(time_snap_list)
+            time_snap = ''
 
-	    time_snap_list = [s_item for s_item in os.listdir(submission_source) \
-                if os.path.isdir(os.path.join(submission_source, s_item))]
+            if len( time_snap_list) > 1 :
+                time_snap = max(time_snap_list)
+
+            elif len(time_snap_list) == 1:
+                time_snap = time_snap_list[0]
+
+            if len(time_snap) == 0:
+                continue
+
+            time_snap_dir = os.path.join(submission_source, time_snap)
+
+            submission_file_list = [file_item for file_item in os.listdir(time_snap_dir) \
+            if os.path.exists(os.path.join(time_snap_dir, file_item))]
+
+            #print student_item, ': sub-> filenum: ' , len(submission_file_list)
+
+            submission_name = student_item + '_' +  submission_item + '_'+  time_snap+ '_'
+
+            if len(submission_file_list) >= 1:
+                #print 'folder'
+                student_folder = os.path.join( p_target_dir, submission_name +  'dir_')
+
+                if not os.path.exists(student_folder) :
+                    #print 'creating student folder {0}'.format(student_folder)
+                    os.mkdir(student_folder)
+
+                print len(submission_file_list)
+                for submission_file_item in submission_file_list:
+                    submission_file_name = os.path.join(time_snap_dir, submission_file_item)
+
+                    file_in_target = os.path.join(student_folder, submission_file_item)
+
+                    # print os.path.exists(file_in_target)
+                    # print (file_in_target)
+                    # print student_folder
+                    # print submission_file_name
+
+                    if not os.path.exists(file_in_target):
+                        # print 'creating ' , (file_in_target)
+                        shutil.copy(submission_file_name,student_folder)
+
+                student_mark = os.path.join( student_folder, submission_name + 'mark_.txt')
 
 
-	    #print len(time_snap_list)
-	    time_snap = ''
-	    if len( time_snap_list) > 1 :
-	        time_snap = max(time_snap_list)
-		#print time_snap_list
-		#print 'selected latest' , time_snap
-	    elif len(time_snap_list) == 1:
-		time_snap = time_snap_list[0]
-	
-	    if len(time_snap) == 0:
-		continue
+                make_mark_file(student_mark, student_item, submission_item, time_snap,question_list)
 
-	    time_snap_dir = os.path.join(submission_target, time_snap)
 
-	    if not os.path.exists(time_snap_dir):
-		os.mkdir(time_snap_dir)
-		
-	    mark_file_name = os.path.join(time_snap_dir, 'mark.txt')
-		
-	    file_h = open(mark_file_name, 'w')
-	    
-	    str_header = 'ID:{0}  A#:{1}  @{2}\n'.format(student_item, submission_item, time_snap)
-		
-	    str_seprator = '*'*len(str_header) + '\n'
-	    half_len = len(str_header) / 2
-	    str_half_sep = '-'* half_len + '\n'
-	    str_half_space = ' '*half_len
 
-	    file_h.write(str_seprator)
-	    file_h.write(str_header)
 
-	    file_h.write(str_seprator)
-
-	    for q in question_list:
-		str_q = '{2}{0}\n{1}'.format(q[0], str_seprator, str_half_space)
-		file_h.write(str_q)	
-
-		for part in q[1]:
-		    str_part = '({0})\n\n'.format(part) 
-		    file_h.write(str_part)   
-
-		file_h.write(str_seprator)
-
-	    #for q in range(0, p_questions):
-		#str_q = 'Q{0}:\n\n{1}'.format(q+1, str_seprator)
-		#file_h.write(str_q)
-		
-	    str_total = 'sum = \n{0}'.format(str_seprator)
-	    file_h.write(str_total)
-	    file_h.close()
-	
 #        print student_submission_list
 
 if __name__ == '__main__':
     argv = sys.argv[1:]
-    print argv
+    # argv
+
 
     main(argv[0], argv[1], argv[2])
--- sum_mark_dir.py
+++ sum_mark_dir.py
@@ -1,208 +1,218 @@
 import sys, os
 
 def read_question_parts(p_file_name):
-	file_h = open(p_file_name)
-	lines_read = file_h.readlines()
-	q_list = [] 
-	for q in lines_read:
-		q_split = q.split(':')
-		question =  q_split[0]
 
-		parts = q_split[1].split()
-	#print parts
-	
-		q_list.append([question, parts])
+    file_name_dir = os.path.join(os.getcwd(), p_file_name)
+    file_h = open(file_name_dir)
 
-	file_h.close()
-	return q_list
+    lines_read = file_h.readlines()
+    q_list = []
+    for q in lines_read:
+        q_split = q.split(':')
+        question =  q_split[0]
+
+        parts = q_split[1].split()
+    #print parts
+
+        q_list.append([question, parts])
+
+
+    file_h.close()
+    return q_list
 
 def main(p_source_dir, p_target_dir, p_q_file, p_all_mark_file):
 
-	p_source_dir = os.path.abspath(p_source_dir)
-	p_target_dir = os.path.abspath(p_target_dir)
-	
-	p_questions = 8
-	question_list = read_question_parts(p_q_file)
+    p_source_dir = os.path.abspath(p_source_dir)
+    p_target_dir = os.path.abspath(p_target_dir)
 
-	#print question_list
+    p_questions = 8
+    question_list = read_question_parts(p_q_file)
 
-	if not os.path.exists(p_target_dir):
-		print 'creating path', p_target_dir
-		os.mkdir(p_target_dir)
+    #print question_list
 
-	
+    if not os.path.exists(p_target_dir):
+        print 'creating path', p_target_dir
+        os.mkdir(p_target_dir)
 
-	student_list = [item_prop for item_prop in os.listdir(p_source_dir)
-					if os.path.isdir(os.path.join(p_source_dir, item_prop))]    
-	#print student_list
-	all_mark_file_name = os.path.abspath(p_all_mark_file)
-	print all_mark_file_name
-	file_all_h = open(all_mark_file_name, 'w')
 
-	for student_item in student_list:
-		student_path = os.path.join(p_target_dir, student_item)
-		student_source_path = os.path.join(p_source_dir, student_item)
 
-		if not os.path.exists(student_path):
+    student_list = [item_prop for item_prop in os.listdir(p_source_dir)
+                    if os.path.isdir(os.path.join(p_source_dir, item_prop))]
+    #print student_list
+    all_mark_file_name = os.path.abspath(p_all_mark_file)
+    print all_mark_file_name
+    file_all_h = open(all_mark_file_name, 'w')
 
-			os.mkdir(student_path)
+    for student_item in student_list:
+        student_path = os.path.join(p_target_dir, student_item)
+        student_source_path = os.path.join(p_source_dir, student_item)
+
+        if not os.path.exists(student_path):
+
+            os.mkdir(student_path)
 
 #        print student_source_path 
-		student_submission_list = [s_item for s_item in os.listdir(student_source_path) \
-		if os.path.isdir(os.path.join(student_source_path, s_item))]
+        student_submission_list = [s_item for s_item in os.listdir(student_source_path) \
+        if os.path.isdir(os.path.join(student_source_path, s_item))]
 
 
-		for submission_item in student_submission_list :
-			submission_source = os.path.join(student_source_path, submission_item)
+        for submission_item in student_submission_list :
+            submission_source = os.path.join(student_source_path, submission_item)
 
-			submission_target = os.path.join(student_path, submission_item)
+            submission_target = os.path.join(student_path, submission_item)
 
-			if not os.path.exists(submission_target):
-				os.mkdir(submission_target)
+            if not os.path.exists(submission_target):
+                os.mkdir(submission_target)
 
-			time_snap_list = [s_item for s_item in os.listdir(submission_source) \
-					if os.path.isdir(os.path.join(submission_source, s_item))]
-
-
-			#print len(time_snap_list)
-			time_snap = ''
-			if len( time_snap_list) > 1 :
-				time_snap = max(time_snap_list)
-			
-			elif len(time_snap_list) == 1:
-				time_snap = time_snap_list[0]
-		
-			if len(time_snap) == 0:
-				continue
-
-			
-			time_snap_source_file = os.path.join(submission_source, time_snap, 'mark.txt')
-
-			file_h_in = open(time_snap_source_file, 'r')
-			
-			score_lines = file_h_in.readlines()
-			file_h_in.close()
-
-			time_snap_dir = os.path.join(submission_target, time_snap)
-
-			if not os.path.exists(time_snap_dir):
-				os.mkdir(time_snap_dir)
-			
-			mark_file_name = os.path.join(time_snap_dir, 'mark.txt')
-			
-			file_h = open(mark_file_name, 'w')
-			
+            time_snap_list = [s_item for s_item in os.listdir(submission_source) \
+                    if os.path.isdir(os.path.join(submission_source, s_item))]
 
 
-			#str_header = score_lines[0:2]
+            #print len(time_snap_list)
+            time_snap = ''
+            if len( time_snap_list) > 1 :
+                time_snap = max(time_snap_list)
 
-			
-			#for h_item in str_header:
-			#	file_h.write(h_item)
+            elif len(time_snap_list) == 1:
+                time_snap = time_snap_list[0]
 
-			#str_footer = score_lines[-3:-1]
-
-			scores_pure = score_lines[2:]
-
-			#file_h.write(str(scores_pure))
-
-			#str_seprator = score_lines[0]
-			i = 0
-			sum_score = 0
-			sum_count = 0
-			
-			#print question_list
-
-			for q in question_list:
-
-				str_q = scores_pure[i]
-				i+= 1
-				while str_q.find('*') < 0:
-					
-					str_q = scores_pure[i]
-					i += 1
-					#print scores_pure[i], 'Q0'
-
-				#file_h.write(str_q)
-
-				str_q = scores_pure[i]
-				i+= 1
-				while str_q.find(str(q[0])) < 0:
-
-					str_q = scores_pure[i]
-					i += 1
-					#print str_q, 'Q1'
-
-				#file_h.write(str_q)
-
-				str_q = scores_pure[i]
-				i+= 1
-				while str_q.find('*') < 0:
-					str_q = scores_pure[i]
-					i += 1
-					#print str_q, 'Q2'
-				#file_h.write(str_q)
+            if len(time_snap) == 0:
+                continue
 
 
-				if len(q[1]) > 0:
-					#print i, scores_pure[i]
-					for part in q[1]:
+            time_snap_source_file = os.path.join(submission_source, time_snap, 'mark.txt')
 
-						str_part = scores_pure[i]
-						#file_h.write(str_part)
-						i += 1
-						
-						str_score =  scores_pure[i]
-						#print str(str_score.split())
-						sum_score += float(str_score)
-						sum_count += 1
-						#file_h.write(str_score)
-						i +=  1  
+            file_h_in = open(time_snap_source_file, 'r')
 
+            score_lines = file_h_in.readlines()
+            file_h_in.close()
 
-				elif len(q[1]) == 0:
-					str_score =  scores_pure[i]
-					sum_score += float(str_score)
-					sum_count += 1
-					#file_h.write(str_score)
-					#print 'berf 3 ', scores_pure[i -1]
+            time_snap_dir = os.path.join(submission_target, time_snap)
 
-					i +=  1 
-					#print 'after 3 ', scores_pure[i]
+            if not os.path.exists(time_snap_dir):
+                os.mkdir(time_snap_dir)
 
-				#str_end_q =  scores_pure[i]
-				#i+= 1
-				#print i, scores_pure[i]
-				#file_h.write(str_end_q)
+            mark_file_name = os.path.join(time_snap_dir, 'mark.txt')
+
+            file_h = open(mark_file_name, 'w')
 
 
-			if sum_count != 8:
-				print 'invalid'
+
+            #str_header = score_lines[0:2]
 
 
-			str_total = 'sum = {1}/{0}\n'.format(sum_count, sum_score)
-			
-			for s_line in score_lines:
-				if s_line.find('sum') < 0:
-					file_h.write(s_line)
-				else:
-					file_h.write(str_total)
+            #for h_item in str_header:
+            #	file_h.write(h_item)
 
-			#file_h.write(str_total)
+            #str_footer = score_lines[-3:-1]
 
-			file_h.close()
-			student_id = student_item.split('-')
-			str_for_all = '{2} = {1}/{0}\n'.format(sum_count, sum_score, student_id[0])
-			file_all_h.write(str_for_all)
+            scores_pure = score_lines[2:]
 
-	file_all_h.close()
+            #file_h.write(str(scores_pure))
+
+            #str_seprator = score_lines[0]
+            i = 0
+            sum_score = 0
+            sum_count = 0
+
+            #print question_list
+
+            for q in question_list:
+
+                str_q = scores_pure[i]
+                i+= 1
+                while str_q.find('*') < 0:
+
+                    str_q = scores_pure[i]
+                    i += 1
+                    #print scores_pure[i], 'Q0'
+
+                #file_h.write(str_q)
+
+                str_q = scores_pure[i]
+                i+= 1
+                while str_q.find(str(q[0])) < 0:
+
+                    str_q = scores_pure[i]
+                    i += 1
+                    #print str_q, 'Q1'
+
+                #file_h.write(str_q)
+
+                str_q = scores_pure[i]
+                i+= 1
+                while str_q.find('*') < 0:
+                    str_q = scores_pure[i]
+                    i += 1
+                    #print str_q, 'Q2'
+                #file_h.write(str_q)
 
 
-	
+                if len(q[1]) > 0:
+                    #print i, scores_pure[i]
+                    for part in q[1]:
+
+                        str_part = scores_pure[i]
+                        #file_h.write(str_part)
+                        i += 1
+
+                        str_score =  scores_pure[i]
+                        #print str(str_score.split())
+                        sum_score += float(str_score)
+                        sum_count += 1
+                        #file_h.write(str_score)
+                        i +=  1
+
+
+                elif len(q[1]) == 0:
+                    str_score =  scores_pure[i]
+                    sum_score += float(str_score)
+                    sum_count += 1
+                    #file_h.write(str_score)
+                    #print 'berf 3 ', scores_pure[i -1]
+
+                    i +=  1
+                    #print 'after 3 ', scores_pure[i]
+
+                #str_end_q =  scores_pure[i]
+                #i+= 1
+                #print i, scores_pure[i]
+                #file_h.write(str_end_q)
+
+
+            if sum_count != 8:
+                print 'invalid'
+
+
+            str_total = 'sum = {1}/{0}\n'.format(sum_count, sum_score)
+
+            for s_line in score_lines:
+                if s_line.find('sum') < 0:
+                    file_h.write(s_line)
+                else:
+                    file_h.write(str_total)
+
+            str_correction = 'This is .docx format in case you have difficulty reading .txt\n Nothing changed.\n'
+            file_h.write(str_correction)
+            file_h.close()
+
+            os.system('textutil -convert docx {0} '.format(mark_file_name))
+
+
+            student_id = student_item.split('-')
+            str_for_all = '{2} = {1}/{0}\n'.format(sum_count, sum_score, student_id[0])
+            file_all_h.write(str_for_all)
+
+
+
+    file_all_h.close()
+
+
+
 #        print student_submission_list
 
 if __name__ == '__main__':
-	argv = sys.argv[1:]
-	print argv
+    argv = sys.argv[1:]
+    print argv
 
-	main(argv[0], argv[1], argv[2], argv[3])
+    main(argv[0], argv[1], argv[2], argv[3])
