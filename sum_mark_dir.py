049ebbfa37a7fef767a51715e8f690cb532a109e
ameri <mo_amer@encs.concordia.ca>
2015-03-07 00:25:47 -0500

    removed un necessary project file .idea it is working with all in one folder format

--- sum_mark_dir.py
+++ sum_mark_dir.py
@@ -1,5 +1,12 @@
 import sys, os
 
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
+
 def read_question_parts(p_file_name):
 
     file_name_dir = os.path.join(os.getcwd(), p_file_name)
@@ -20,6 +27,108 @@ def read_question_parts(p_file_name):
     file_h.close()
     return q_list
 
+
+def sum_student_mark_(p_source_file, p_target_file, p_question_list):
+
+    file_h_in = open(p_source_file, 'r')
+
+    score_lines = file_h_in.readlines()
+    file_h_in.close()
+
+
+
+
+    i = 0
+    sum_score = 0.0
+    sum_count = 0.0
+
+
+    question_header = [ [question_item, str_line, score_lines.index(str_line)]\
+                        for question_item in p_question_list  for str_line in score_lines \
+                     if question_item[0] in str_line]
+
+
+
+    for question_item in question_header:
+
+        start_i = question_item[2] + 1
+
+        q_parts = []
+        if len (question_item[0]) == 2:
+            q_parts = question_item[0][1]
+
+        ##start of question
+        cur_i = start_i
+        while score_lines[cur_i].find('*') >= 0:
+            cur_i += 1
+        start_i = cur_i
+
+        # end of question
+        cur_i = start_i
+        while score_lines[cur_i].find('*') < 0:
+            cur_i += 1
+        end_i = cur_i
+
+        if len(q_parts) == 0:
+            sum_count +=  1
+            not_valid = True
+            for cur_i in range(start_i, end_i):
+                if len ( score_lines[cur_i].rstrip()) > 0 and isfloat (score_lines[cur_i].rstrip() ):
+                    sum_score += float(score_lines[cur_i].rstrip())
+                    not_valid= False
+                    break
+
+            if not_valid :
+                print 'invalid data in ', score_lines[1], question_item[0]
+
+
+        else:
+            cur_i = start_i
+            for q_part in q_parts:
+                sum_count +=  1
+
+
+                while score_lines[cur_i].find(q_part) < 0 and cur_i <= end_i:
+                    cur_i += 1
+
+                if score_lines[cur_i].find(q_part) < 0 :
+                    print 'invalid data in ', score_lines[1], question_item[0], q_part
+                    break
+
+
+                while not isfloat(score_lines[cur_i].rstrip() )  and cur_i <= end_i:
+                    # print score_lines[cur_i],  q_part, question_item
+                    cur_i += 1
+
+                if isfloat(score_lines[cur_i].rstrip() ):
+                    # print score_lines[cur_i],  q_part, question_item
+                    sum_score += float(score_lines[cur_i].rstrip())
+                else:
+                    print 'invalid data in ', score_lines[1], question_item[0], q_part
+                    break
+
+
+    file_h = open(p_target_file, 'w')
+    for score_line in score_lines:
+        if score_line.find('sum') >= 0:
+            break
+        file_h.write(score_line.replace('*', '_'))
+
+    str_score = 'sum  = {1:3} /  {0:3}\n'.format(sum_count, sum_score)
+
+    file_h.write(str_score)
+
+    file_h.close()
+
+    os.system('textutil -convert html {0} '.format(p_target_file))
+
+
+    print '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+    return [sum_score, sum_count]
+
+
+
+
 def main(p_source_dir, p_target_dir, p_q_file, p_all_mark_file):
 
     p_source_dir = os.path.abspath(p_source_dir)
@@ -28,7 +137,7 @@ def main(p_source_dir, p_target_dir, p_q_file, p_all_mark_file):
     p_questions = 8
     question_list = read_question_parts(p_q_file)
 
-    #print question_list
+    print question_list
 
     if not os.path.exists(p_target_dir):
         print 'creating path', p_target_dir
@@ -38,18 +147,21 @@ def main(p_source_dir, p_target_dir, p_q_file, p_all_mark_file):
 
     student_list = [item_prop for item_prop in os.listdir(p_source_dir)
                     if os.path.isdir(os.path.join(p_source_dir, item_prop))]
-    #print student_list
+    print len(student_list)
+
+
+    # all students
     all_mark_file_name = os.path.abspath(p_all_mark_file)
     print all_mark_file_name
     file_all_h = open(all_mark_file_name, 'w')
 
+    ## for each student
     for student_item in student_list:
-        student_path = os.path.join(p_target_dir, student_item)
+        student_target_path = os.path.join(p_target_dir, student_item)
         student_source_path = os.path.join(p_source_dir, student_item)
 
-        if not os.path.exists(student_path):
-
-            os.mkdir(student_path)
+        if not os.path.exists(student_target_path):
+            os.mkdir(student_target_path)
 
 #        print student_source_path 
         student_submission_list = [s_item for s_item in os.listdir(student_source_path) \
@@ -57,9 +169,9 @@ def main(p_source_dir, p_target_dir, p_q_file, p_all_mark_file):
 
 
         for submission_item in student_submission_list :
-            submission_source = os.path.join(student_source_path, submission_item)
 
-            submission_target = os.path.join(student_path, submission_item)
+            submission_source = os.path.join(student_source_path, submission_item)
+            submission_target = os.path.join(student_target_path, submission_item)
 
             if not os.path.exists(submission_target):
                 os.mkdir(submission_target)
@@ -82,125 +194,18 @@ def main(p_source_dir, p_target_dir, p_q_file, p_all_mark_file):
 
             time_snap_source_file = os.path.join(submission_source, time_snap, 'mark.txt')
 
-            file_h_in = open(time_snap_source_file, 'r')
-
-            score_lines = file_h_in.readlines()
-            file_h_in.close()
-
             time_snap_dir = os.path.join(submission_target, time_snap)
-
             if not os.path.exists(time_snap_dir):
                 os.mkdir(time_snap_dir)
 
             mark_file_name = os.path.join(time_snap_dir, 'mark.txt')
 
-            file_h = open(mark_file_name, 'w')
 
-
-
-            #str_header = score_lines[0:2]
-
-
-            #for h_item in str_header:
-            #	file_h.write(h_item)
-
-            #str_footer = score_lines[-3:-1]
-
-            scores_pure = score_lines[2:]
-
-            #file_h.write(str(scores_pure))
-
-            #str_seprator = score_lines[0]
-            i = 0
-            sum_score = 0
-            sum_count = 0
-
-            #print question_list
-
-            for q in question_list:
-
-                str_q = scores_pure[i]
-                i+= 1
-                while str_q.find('*') < 0:
-
-                    str_q = scores_pure[i]
-                    i += 1
-                    #print scores_pure[i], 'Q0'
-
-                #file_h.write(str_q)
-
-                str_q = scores_pure[i]
-                i+= 1
-                while str_q.find(str(q[0])) < 0:
-
-                    str_q = scores_pure[i]
-                    i += 1
-                    #print str_q, 'Q1'
-
-                #file_h.write(str_q)
-
-                str_q = scores_pure[i]
-                i+= 1
-                while str_q.find('*') < 0:
-                    str_q = scores_pure[i]
-                    i += 1
-                    #print str_q, 'Q2'
-                #file_h.write(str_q)
-
-
-                if len(q[1]) > 0:
-                    #print i, scores_pure[i]
-                    for part in q[1]:
-
-                        str_part = scores_pure[i]
-                        #file_h.write(str_part)
-                        i += 1
-
-                        str_score =  scores_pure[i]
-                        #print str(str_score.split())
-                        sum_score += float(str_score)
-                        sum_count += 1
-                        #file_h.write(str_score)
-                        i +=  1
-
-
-                elif len(q[1]) == 0:
-                    str_score =  scores_pure[i]
-                    sum_score += float(str_score)
-                    sum_count += 1
-                    #file_h.write(str_score)
-                    #print 'berf 3 ', scores_pure[i -1]
-
-                    i +=  1
-                    #print 'after 3 ', scores_pure[i]
-
-                #str_end_q =  scores_pure[i]
-                #i+= 1
-                #print i, scores_pure[i]
-                #file_h.write(str_end_q)
-
-
-            if sum_count != 8:
-                print 'invalid'
-
-
-            str_total = 'sum = {1}/{0}\n'.format(sum_count, sum_score)
-
-            for s_line in score_lines:
-                if s_line.find('sum') < 0:
-                    file_h.write(s_line)
-                else:
-                    file_h.write(str_total)
-
-            str_correction = 'This is .docx format in case you have difficulty reading .txt\n Nothing changed.\n'
-            file_h.write(str_correction)
-            file_h.close()
-
-            os.system('textutil -convert docx {0} '.format(mark_file_name))
+            [student_grade, sum_count ]= sum_student_mark_(time_snap_source_file, mark_file_name, question_list)
 
 
             student_id = student_item.split('-')
-            str_for_all = '{2} = {1}/{0}\n'.format(sum_count, sum_score, student_id[0])
+            str_for_all = '{2} = {1:3} /  {0:3}\n'.format(sum_count, student_grade, student_id[0])
             file_all_h.write(str_for_all)
 
 
@@ -213,6 +218,10 @@ def main(p_source_dir, p_target_dir, p_q_file, p_all_mark_file):
 
 if __name__ == '__main__':
     argv = sys.argv[1:]
-    print argv
 
-    main(argv[0], argv[1], argv[2], argv[3])
+    if len(argv) < 4 :
+        print 'sum_mark_dir.py  source_dir target_dir, marking_part_file, all_mark_for_teacher.txt'
+        print argv
+
+    else:
+        main(argv[0], argv[1], argv[2], argv[3])
---
+++ transfer_to_eas_format.py
@@ -0,0 +1,103 @@
+__author__ = 'ameri'
+
+import sys, os
+
+import shutil
+
+
+
+def main(p_all_in_one_dir, p_eas_format_dir):
+    # print p_all_in_one_dir
+    # print p_eas_format_dir
+
+    p_source_dir = os.path.abspath(p_all_in_one_dir)
+    p_target_dir = os.path.abspath(p_eas_format_dir)
+
+    if not os.path.exists(p_source_dir):
+        print 'soruce does not exist. Quit!'
+        return
+
+    if not os.path.exists(p_target_dir):
+        print 'creating target path: ', p_target_dir
+        os.mkdir(p_target_dir)
+
+    student_list = [item_student for item_student in os.listdir(p_source_dir)
+                    if os.path.isdir(os.path.join(p_source_dir, item_student))]
+
+    print len(student_list)
+
+    for student_item in student_list:
+        #print student_item
+        student_info = student_item.replace('_dir_', '', 1).split('_')
+
+        #print student_info
+
+        if len(student_info) != 5:
+            print 'invalid' , student_item
+            continue
+
+        student_id = student_info[0] + '_' + student_info[1]
+
+        student_A_num = student_info[2] + '_' +  student_info[3]
+
+        student_time = student_info[4]
+
+        mark_file_dir  = os.path.join(p_source_dir, student_item)
+        mark_file_name = student_id + '_' +student_A_num + '_' + student_time + '_mark_.txt'
+
+        mark_file_full_path_with_id = mark_file_dir + '/' + mark_file_name
+
+        mark_file_full_path = mark_file_dir + '/' + 'mark.txt'
+
+        # print mark_file_name
+
+        if not os.path.exists(mark_file_full_path_with_id):
+            print 'invalid file', mark_file_full_path_with_id
+            continue
+
+        id_dir = os.path.join( p_target_dir, student_id)
+        if not os.path.exists(id_dir):
+            os.mkdir(id_dir)
+
+        A_num_dir = os.path.join( id_dir, student_A_num)
+        if not os.path.exists(A_num_dir):
+            os.mkdir(A_num_dir)
+
+        time_dir = os.path.join( A_num_dir, student_time)
+        if not os.path.exists(time_dir):
+            os.mkdir(time_dir)
+
+        shutil.copy(mark_file_full_path_with_id,mark_file_full_path)
+
+        shutil.move(mark_file_full_path, time_dir)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    # argv
+
+    if len(argv) < 2:
+        print 'param needed'
+        print '-> transfer_to_eas_format.py source_dir target_dir'
+
+    else:
+
+        main(argv[0], argv[1])
